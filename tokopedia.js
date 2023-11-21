a5fe5567b351211ccab722617bcb3035d273e230
Dimas <78547184+dimasab@users.noreply.github.com>
2023-11-21 14:58:16 +0700

    Update tokopedia.js: benerin bug [xi] di tokopedia lagi yang ternyata masih ada (harusnya [wi])

--- tokopedia.js
+++ tokopedia.js
@@ -1519,7 +1519,7 @@ app.get('/scrapesemua', async (req, res) => {
                     var hargaraw = satuproduk[wi].querySelector(".css-pp6b3e span").textContent; //harga produk
                     var urlgambar = satuproduk[wi].querySelector(".css-16vw0vn img.success").src.split('?')[0]; // url gambar
                     if (satuproduk[wi].querySelector(".css-1riykrk")) {//jumlah terjual
-                        var terjualraw = satuproduk[xi].querySelector(".css-1riykrk").textContent.trim();
+                        var terjualraw = satuproduk[wi].querySelector(".css-1riykrk").textContent.trim();
                     } else {
                         var terjualraw = Math.floor(Math.random() * 50);
                     }
